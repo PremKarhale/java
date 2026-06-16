01dee03bee3d2b98b6cfb6177c7923886e5a6bbf
23113014-cloud <23113014@pvgcoet.ac.in>
2026-06-16 09:55:34 +0530

    java basic

--- Firstclass.java
+++ Firstclass.java
@@ -1,5 +1,17 @@
 class Firstclass{
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        // printing star pattern 
+        //  *
+        //  ** 
+        //  *** 
+        //  ****
+        System.out.print("*\n**\n***\n****");    
+    
+
     }
 }
+
+
+
