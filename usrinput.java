ed2a1880191abb4afee3ee4a34da45409029e12d
23113014-cloud <23113014@pvgcoet.ac.in>
2026-06-17 22:20:31 +0530

    Create usrinput.java

---
+++ usrinput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class usrinput{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in); //scanner call 
+        String name = sc.nextLine();
+        //
+        System.out.println(name);
+
+    }
+
+}
