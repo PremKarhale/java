710c6fe5432a48e12218dbb1ad65cd3612288009
23113014-cloud <23113014@pvgcoet.ac.in>
2026-06-18 16:45:16 +0530

    Update usrinput.java

--- usrinput.java
+++ usrinput.java
@@ -3,9 +3,24 @@ import java.util.*;
 class usrinput{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in); //scanner call 
-        String name = sc.nextLine();
+        // String name = sc.nextLine();
         //
-        System.out.println(name);
+        // System.out.print("Enter your age :"); 
+
+        // if else statements 
+        // int age = sc.nextInt();
+        // if (age > 18){
+        //     System.out.println("You are adult");
+        // }else{
+        //     System.out.println("No you are not a adult , you are a small kid !");
+        // }
+        int a = sc.nextInt();
+        if (a%2==0){
+            System.out.println("it is even");
+        }
+        else{
+            System.out.println("Odd");
+        }
 
     }
 
