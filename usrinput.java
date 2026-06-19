572b53d5b8387bb6dc3c71cd483bdf2606716a7e
23113014-cloud <premkarhale383@gmail.com>
2026-06-19 19:48:10 +0530

    Update usrinput.java

--- usrinput.java
+++ usrinput.java
@@ -14,21 +14,27 @@ class usrinput{
         //     System.out.println("No you are not a adult , you are a small kid !");
         // }
 
-        System.out.print("Enter the no a:");
-        int a  = sc.nextInt();
-        System.out.print("Enter the no b :");
-        int b = sc.nextInt();
-        if (a==b){
-            System.out.println("Equal");
-        }else if(a>b) {
-            System.out.println("a is greater so we do addition");
-            int sum = a+b;
-            System.out.println("its sum is : "+ sum);
-        }else{
-            System.out.println("a is smaller so we do subtraction");
-            int sub = a-b;
-            System.out.println("its sub is :"+sub);
+        // System.out.print("Enter the no a:");
+        // int a  = sc.nextInt();
+        // System.out.print("Enter the no b :");
+        // int b = sc.nextInt();
+        // if (a==b){
+        //     System.out.println("Equal");
+        // }else if(a>b) {
+        //     System.out.println("a is greater so we do addition");
+        //     int sum = a+b;
+        //     System.out.println("its sum is : "+ sum);
+        // }else{
+        //     System.out.println("a is smaller so we do subtraction");
+        //     int sub = a-b;
+        //     System.out.println("its sub is :"+sub);
 
+        // }
+
+        switch(btn){
+            case 1 :System.out.println("hello");
+            break;
+            case 2 : sysout
         }
     }
     
