d2c4eca9ead81272b839a60d2ed68bb4b69ec5ee
23113014-cloud <premkarhale383@gmail.com>
2026-06-19 18:52:40 +0530

    Update usrinput.java

--- usrinput.java
+++ usrinput.java
@@ -3,10 +3,9 @@ import java.util.*;
 class usrinput{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in); //scanner call 
-        // String name = sc.nextLine();
-        //
-        // System.out.print("Enter your age :"); 
 
+        // String name = sc.nextLine();
+        // System.out.print("Enter your age :"); 
         // if else statements 
         // int age = sc.nextInt();
         // if (age > 18){
@@ -14,14 +13,23 @@ class usrinput{
         // }else{
         //     System.out.println("No you are not a adult , you are a small kid !");
         // }
-        int a = sc.nextInt();
-        if (a%2==0){
-            System.out.println("it is even");
-        }
-        else{
-            System.out.println("Odd");
-        }
 
+        System.out.print("Enter the no a:");
+        int a  = sc.nextInt();
+        System.out.print("Enter the no b :");
+        int b = sc.nextInt();
+        if (a==b){
+            System.out.println("Equal");
+        }else if(a>b) {
+            System.out.println("a is greater so we do addition");
+            int sum = a+b;
+            System.out.println("its sum is : "+ sum);
+        }else{
+            System.out.println("a is smaller so we do subtraction");
+            int sub = a-b;
+            System.out.println("its sub is :"+sub);
+
+        }
     }
     
 
