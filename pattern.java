5b2965f07571bf387fc0e6aff5c992f16d12078e
23113014-cloud <premkarhale383@gmail.com>
2026-07-07 18:19:32 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -37,9 +37,20 @@ class pattern{
     // }
 
     // 4 ) inverted pyramid
-        for(int i =1;i<=4;i++){
-            for(int j = 5; j>i ;j--){
-                System.out.print("*");
+        // for(int i =1;i<=4;i++){
+        //     for(int j = 5; j>i ;j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // 5) inverted half pyramid
+        for (int i =1; i<=4; i ++){
+            for(int j =1;j<=4-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("8");
             }
             System.out.println();
         }
