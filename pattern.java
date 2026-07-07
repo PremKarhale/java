6d69f899b91bcc72dd865253358b9f06c6d71be8
23113014-cloud <premkarhale383@gmail.com>
2026-07-07 18:14:22 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -10,16 +10,40 @@ class pattern{
         //    System.out.println(""); 
         //   }
 
-        for(int i=1;i<=4;i++){
-            for(int j=1;j<=5;j++){
-                if(i==2){
-                    if(j==2|j==3|j==4){
-                        System.out.print("");                       
-                    }
-                }
-                System.out.print("*");                
+
+        // next pattern this :
+        // *****
+        // *   *
+        // *   *
+        // *****
+
+    //     for(int i=1;i<=4;i++){
+    //         for(int j=1;j<=5;j++){
+    //             if (i==1||i==4 || j==1 || j==5 ){
+    //                 System.out.print("*");
+    //             }else{
+    //                 System.out.print(" ");
+    //             }              
+    //         }
+    //         System.out.println("");
+    // }
+
+    // 3) half pyrimid
+    // for(int i=1;i<=4;i++){
+    //     for(int j =1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+    // 4 ) inverted pyramid
+        for(int i =1;i<=4;i++){
+            for(int j = 5; j>i ;j--){
+                System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
         }
+
+
     }
 }
