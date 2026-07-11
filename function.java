861dd0b89f330d7b2e8e7acd2b7f5350727078dc
23113014-cloud <premkarhale383@gmail.com>
2026-07-11 16:28:39 +0530

    first function in java

---
+++ function.java
@@ -0,0 +1,15 @@
+
+public class function{
+    public static void printMyName(String name){
+        System.out.println(name);    //first function
+        return;
+    }
+
+
+
+
+
+    public static void main (String args[]){
+        
+    }
+}
