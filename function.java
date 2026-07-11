09a97ece3a1e4cd4912541dd53de8f57c856f09a
23113014-cloud <premkarhale383@gmail.com>
2026-07-11 19:24:28 +0530

    Factorial code in java

--- function.java
+++ function.java
@@ -12,16 +12,32 @@ public class function{
     // }
 
     // *Product
-    public static int product(int a , int b ){
-        int product = a*b;
-        return product;
+    // public static int product(int a , int b ){
+    //     int product = a*b;
+    //     return product;
+    // }
+
+    // *Factorial
+    public static void factorial(int a){
+        if (a<0){
+            return;
+        }
+        int fact =1;
+        for(int i=a;i>=1;i--){
+             fact = fact*i;
+        }
+        System.out.println("Factorail on the no."+ a +"is : "+ fact);
+
     }
 
 
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println(product(a,b));
+        System.out.print("Enter the no who you want to find the factorial :");
         int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(product(a,b));
+        factorial(a);
     }
 }
