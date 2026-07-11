4a7cdd7944ab2475b05cb6b9d56596d45a84e32f
23113014-cloud <premkarhale383@gmail.com>
2026-07-11 18:50:12 +0530

    java functions

--- function.java
+++ function.java
@@ -1,15 +1,27 @@
+import java.util.*;
 
 public class function{
-    public static void printMyName(String name){
-        System.out.println(name);    //first function
-        return;
+    // public static void printMyName(String name){
+    //     System.out.println(name);    //first function
+    //     return;
+    // }
+
+    // * sum
+    // public static int sum(int a,int b){
+    //     return a+b;
+    // }
+
+    // *Product
+    public static int product(int a , int b ){
+        int product = a*b;
+        return product;
     }
 
 
-
-
-
     public static void main (String args[]){
-        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(product(a,b));
     }
 }
