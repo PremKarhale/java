da67df0373021caa28de86d078a158e3fc4261f5
23113014-cloud <premkarhale383@gmail.com>
2026-07-12 11:40:26 +0530

    Update functions2.java

--- functions2.java
+++ functions2.java
@@ -8,7 +8,7 @@ public class functions2 {
     // }
     
     public static void oddSum(int n){
-        int sum =1;
+        int sum =0;
         for(int i=1;i<=n;i++){
             if(i%2!=0){
                 sum=sum+i;
@@ -19,9 +19,11 @@ public class functions2 {
 
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        oddSum(5);
+        while(true){
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            oddSum(n);
+        }
 
 
     }
