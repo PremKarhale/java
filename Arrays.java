3f90583eb41f57d03d794c3c731f1d5c2ac6599e
23113014-cloud <premkarhale383@gmail.com>
2026-07-13 16:47:14 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -6,9 +6,12 @@ public class Arrays {
         marks[0]=1;
         marks[1]=2;
         marks[2]=3;
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        for(int i=0;i<4;i++){
+            System.out.println(marks[i]);
+        }
 
     }
 }
