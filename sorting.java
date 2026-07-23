f969106a2ee6c68b95154b2054a2d2491027724e
23113014-cloud <premkarhale383@gmail.com>
2026-07-23 23:55:26 +0530

    Update sorting.java

--- sorting.java
+++ sorting.java
@@ -6,16 +6,31 @@ public class sorting {
         }
     }
     public static void main(String[] args) {
-        // Bubble sort 
-        int arr[]={7,8,3,1,2};
+        // // Bubble sort 
+        // int arr[]={7,8,3,1,2};
+        // for(int i=0;i<arr.length-1;i++){
+        //     for(int j=0;j<arr.length-i-1;j++){
+        //         if(arr[j] > arr[j+1]){
+        //             int temp = arr[j];
+        //             arr[j]=arr[j+1];
+        //             arr[j+1]=temp;
+        //         }
+        //     }
+        // }
+        // printArray(arr);
+
+        // selection sort
+        int arr[] ={7,8,3,1,2};
         for(int i=0;i<arr.length-1;i++){
-            for(int j=0;j<arr.length-i-1;j++){
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+            int smallest = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[smallest]>arr[j]){
+                    smallest=j;
                 }
-            }
+            }   
+            int temp = arr[smallest];
+            arr[smallest]=arr[i];
+            arr[i]=temp;
         }
         printArray(arr);
     }
