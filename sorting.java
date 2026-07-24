ffd00de47e21cca477851bbd4a7420c3a0c3ed8a
23113014-cloud <premkarhale383@gmail.com>
2026-07-24 23:59:25 +0530

    Update sorting.java

--- sorting.java
+++ sorting.java
@@ -36,20 +36,34 @@ public class sorting {
 
         // finding the second largest element in an array 
         int arr[]={1,2,4,7,7,5};
+        // int largest =arr[0];
+        // for(int i=1;i<arr.length;i++){
+        //     if(arr[i]>largest){
+        //         largest = arr[i];
+        //     }
+        // }
+        // System.out.println(largest);  // i got the largest element !
+        // // now finding the second largest element ;
+        // int secondl=-1;
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]>secondl && arr[i]!=largest){
+        //         secondl = arr[i];
+        //     }
+        // }
+        // System.out.println(secondl);
+
+        // optimal solution 
         int largest =arr[0];
+        int slargest = -1;
         for(int i=1;i<arr.length;i++){
             if(arr[i]>largest){
+                slargest = largest;
                 largest = arr[i];
+            }else if(arr[i]<largest && arr[i]>slargest){
+                slargest = arr[i];
             }
         }
-        System.out.println(largest);  // i got the largest element !
-        // now finding the second largest element ;
-        int secondl=-1;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>secondl && arr[i]!=largest){
-                secondl = arr[i];
-            }
-        }
-        System.out.println(secondl);
+        System.out.println(largest);
+        System.out.println(slargest);
     }
 }
