fcd1cac3d81dd7e4c916f85dc269e15af1c832ff
23113014-cloud <premkarhale383@gmail.com>
2026-08-04 13:20:51 +0530

    Update solutions.java

--- solutions.java
+++ solutions.java
@@ -1,22 +1,29 @@
-import java.util.HashSet;
-
-// Remove duplicates from the array
 public class solutions {
     public int removeDuplicates(int[] nums){
-        HashSet<Integer> seen = new HashSet<>();
-        int index = 0;
-        for(int num:nums){
-            if(!seen.contains(num)){
-                seen.add(num);
-                nums[index] = num;
-                index++;
+        // HashSet<Integer> seen = new HashSet<>();
+        // int index = 0;
+        // for(int num:nums){
+        //     if(!seen.contains(num)){
+        //         seen.add(num);
+        //         nums[index] = num;
+        //         index++;
+        //     }
+        // }
+        // return index; //tell no of unique elements.
+
+        // optimal solution 
+        int i =0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                nums[i+1]=nums[j];
+                i++;
             }
         }
-        return index; //tell no of unique elements.
+        return i+1;
     }
 
     public static void main(String[] args){
-        int nums[]={1,2,1,2,4,3,3,};
+        int nums[]={1,1,2,3,3,};
         solutions sol = new solutions(); 
         int  k =sol.removeDuplicates(nums);
         System.out.println(k);
