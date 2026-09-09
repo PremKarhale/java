fd3371d32fa5e2866b3d0de02f5b36881032d415
23113014-cloud <premkarhale383@gmail.com>
2026-09-09 14:58:53 +0530

    Create StringBasics.java

---
+++ StringBasics.java
@@ -0,0 +1,24 @@
+public class StringBasics {
+    public static void main(String[] args){
+        // String s = "hello";
+        // System.out.println(s);
+        // System.out.println(s.charAt(1));
+        
+        // Q) Count the no of vowels in word "Karhale"
+    //     String k = "Karhale";
+    //     int count =0;
+    //     for(int i=0;i<k.length();i++){
+    //         char ch = k.charAt(i);
+    //         if(ch == 'a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+    //             count ++;
+    //         }
+    //     }
+    //     System.out.println(count);
+    // }
+    
+    // Q ) find the following the sting is palendrome or not 
+        String s = "markram";
+        
+
+
+}
